a75f3ad65ce98b872ecd96c5da2b6db9ba7a4677
willianmenezess <willmenezes@outlook.com.br>
2023-05-05 20:34:43 -0300

    feat: resolve requisito 2 - cria os elementos da tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,45 @@
-import React from 'react';
+// import React, { useContext } from 'react';
 
 function Login() {
+  // const [inputEmail, setInputEmail] = useState('');
+  // const [inputPassword, setInputPassword] = useState('');
+  // const [isDisabled, setIsDisabled] = useState(true);
   return (
-    <div>Login</div>
+    <section>
+      <form>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            data-testid="email-input"
+            // value={ inputEmail }
+            // onChange={ ({ target }) => setInputEmail(target.value) }
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          Senha
+          <input
+            type="password"
+            id="password"
+            data-testid="password-input"
+            // value={ inputPassword }
+            // onChange={ ({ target }) => setInputPassword(target.value) }
+          />
+        </label>
+        <br />
+        <button
+          type="submit"
+          data-testid="login-submit-btn"
+          // disabled={ isDisabled }
+          // onClick={}
+        >
+          Enter
+
+        </button>
+      </form>
+    </section>
   );
 }
 
