0be6e6b3519102982a46e05ac7812c6e0126ef57
Charles <charlesepatricia@hotmail.com>
2023-05-07 10:17:33 -0300

    Footer: Requisitos 16, 17 e 18 - Resolvidos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
+  const history = useHistory();
+
+  const handleClickMealDrink = (name) => {
+    console.log('name');
+    if (name === 'drinks') {
+      history.push('/drinks');
+    }
+    if (name === 'meals') {
+      history.push('/meals');
+    }
+  };
+
   return (
-    <div>Footer</div>
+    <footer
+      data-testid="footer"
+    >
+      <button
+        className="icon"
+        data-testid="drinks-bottom-btn"
+        type="button"
+        src="/static/media/drinkIcon.efc0d3c156e5da73e44a826c8d67b142.svg"
+        onClick={ () => handleClickMealDrink('drinks') }
+      >
+        <img
+          src={ drinkIcon }
+          alt="Icon drink"
+        />
+      </button>
+
+      <button
+        className="icon"
+        data-testid="meals-bottom-btn"
+        type="button"
+        src="/static/media/mealIcon.efc0d3c156e5da73e44a826c8d67b142.svg"
+        onClick={ () => handleClickMealDrink('meals') }
+      >
+        <img
+          src={ mealIcon }
+          alt="Icon meal"
+        />
+      </button>
+    </footer>
   );
 }
 
